146c681ae957eadfc0686d2a2789816a97ae4a22
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2025-02-18 10:35:19 +0700

    Add login and use bcrypt for encrypt password.

--- models/user.go
+++ models/user.go
@@ -1,7 +1,10 @@
 package models
 
 import (
+	"errors"
+
 	"example.com/rest-api/db"
+	"example.com/rest-api/utils"
 )
 
 type User struct {
@@ -22,7 +25,14 @@ func (u *User) Save() error {
 	}
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
+
 	if err != nil {
 		return err
 	}
@@ -34,4 +44,25 @@ func (u *User) Save() error {
 
 	u.ID = userId
 	return nil
+}
+
+func (u User) ValidateCredentials() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&retrievedPassword)
+
+	if err != nil {
+		return err
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+
+	if !passwordIsValid {
+		return errors.New("Credentials invalid")
+	}
+
+	return nil
+
 }
--- routes/users.go
+++ routes/users.go
@@ -25,3 +25,23 @@ func signup(context *gin.Context) {
 	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully", "user": user})
 	
 }
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Could not parse request data.: " + err.Error()})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"error": "Could not authenticate user." + err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+}
