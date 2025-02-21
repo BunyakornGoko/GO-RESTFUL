50757bd5bf26b68cce5c6e8a78737e68a938f4c6
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2025-02-21 17:30:36 +0700

    manage-register-end

--- routes/register.go
+++ routes/register.go
@@ -32,6 +32,26 @@ func registerForEvent(context *gin.Context) {
 	context.JSON(http.StatusCreated, gin.H{"message": "Registered!"})
 }
 
-// func cancelRegistration(context *gin.Context) {
+func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
 
-// }
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	err = event.CancelRegistration(userId)
+	if err != nil {
+		// Log the error for debugging
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel registration.", "error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Registration cancelled."})
+}
